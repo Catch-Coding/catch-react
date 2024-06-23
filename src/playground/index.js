6b8474ab5cfdc305c2c09db43f48034e8e631e72
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-23 18:44:49 -0400

    test

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import Gui from "./components/gui/gui"
+import Gui from "../components/gui/gui"
 
 function Catch() {
     const container = document.getElementById("root");
