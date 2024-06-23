37e07a706a46ba5a7b59f3e9e375ffa36bd19f29
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-23 18:58:19 -0400

    bruh

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/playground/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "build"),
