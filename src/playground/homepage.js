8971d321696760930f9eea66b0c9983eb0c31fcc
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 16:03:36 -0400

    please work

--- src/playground/homepage.js
+++ src/playground/homepage.js
@@ -4,7 +4,12 @@ import { createRoot } from "react-dom/client";
 function Render() {
   const container = document.getElementById("root");
     const root = createRoot(container);
-    root.render(<h1>Incomplete homepage</h1>);
+    root.render(
+      <>
+        <h1>Incomplete homepage</h1>
+        <a href="studio">Just go to the studio for now</a>
+      </>
+    );
 }
 
 Render();
