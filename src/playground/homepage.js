eca7d17efb09df485e37165e4d70af526e6437c6
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 17:28:50 -0400

    change some things

--- src/playground/homepage.js
+++ src/playground/homepage.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Homepage from "../components/homepage/homepage";
 
 function Render() {
   const container = document.getElementById("root");
     const root = createRoot(container);
     root.render(
-      <>
-        <h1>CatchJS Homepage</h1>
-        <hr/>
-        <p>CatchJS is a WIP website for people to learn javascript.</p>
-        <a href="studio">STUDIO</a>
-      </>
+       <Homepage/>
     );
 }
 
