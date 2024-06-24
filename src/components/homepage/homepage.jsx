395d9120582c122d8e8db2686bc76f6db7c47a41
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 17:24:02 -0400

    homepage component

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./homepage.css";
+import LogoSvg from "../../images/logo.svg";
+
+export default function Homepage() {
+  return (
+    <>
+      <nav>
+        <a href="studio">Studio</a>
+      </nav>
+      <h1><img src={LogoSvg}/>CatchJS Homepage</h1>
+      <hr/>
+      <p>CatchJS is a WIP website that can help people learn JavaScript</p>
+      <p>In the future, CatchJS will include docs, tutorials, and an advanced studio.</p>
+      <p>Currently, the website only includes a non-functional studio.</p>
+    </>
+  )
+}
