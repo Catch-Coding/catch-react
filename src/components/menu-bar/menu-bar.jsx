26d44028ef9991c2122f8d535e863ba44995e89c
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 14:28:04 -0400

    idk

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -8,15 +8,15 @@ import React from 'react';
 import './menu-bar.css';
 import LogoSvg from '../../images/logo.svg';
 
-function MenuBar() {
+function MenuBar({newCallback, saveCallback, openCallback, runCallback}) {
     return (
         <nav className="menu-bar">
                 <img src={LogoSvg}/>
                 <input type="text" placeholder="Project Name"></input>
-                <button>New</button>
-                <button>Save</button>
-                <button>Open</button>
-                <button>Run</button>
+                <button onclick={newCallback||()=>{}}>New</button>
+                <button onclick={saveCallback||()=>{}}>Save</button>
+                <button onclick={openCallback||()=>{}}>Open</button>
+                <button onclick={runCallback||()=>{}}>Run</button>
         </nav>
     );
 }
