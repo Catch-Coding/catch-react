11c5481e0c983116c80515871c256ff4f33e6650
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 16:01:38 -0400

    multiple thingies

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,26 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/playground/studio.js",
+    entry: {
+      studio: "./src/playground/studio.js",
+      homepage: "./src/playground/homepage.js"
+    },
     output: {
-        filename: "main.js",
+        filename: "[name].js",
         path: path.resolve(__dirname, "build"),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "public", "index.html"),
+            filename: "studio/index.html",
+            chunks; ["studio"],
             title: "CatchJS Studio"
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+            filename: "index.html",
+            chunks; ["homepage"],
+            title: "CatchJS Home"
         }),
     ],
     module: {
