1020ef32fa9a2e9cdac00a5df54f135341cc0ec6
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 13:40:34 -0400

    change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/playground/index.js",
+    entry: "./src/playground/studio.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "build"),
