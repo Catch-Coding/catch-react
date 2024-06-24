9c1a9d4d8f475f22c9c25014a100dd74e0ba64fd
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-24 14:18:27 -0400

    idk

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -6,10 +6,12 @@
 
 import React from 'react';
 import './menu-bar.css';
+import LogoSvg from '../images/logo.svg';
 
 function MenuBar() {
     return (
         <nav className="menu-bar">
+                <img src={LogoSvg}/>
                 <input type="text" placeholder="Project Name"></input>
                 <button>New</button>
                 <button>Save</button>
