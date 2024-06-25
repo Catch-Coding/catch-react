367adaedf9a7b12d0e98244c37ce6ca08b2a0280
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-25 11:36:33 -0400

    edit + remove table of contents cuz idk how to fix its css

--- src/playground/tutorial.js
+++ src/playground/tutorial.js
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom";
 import TableOfContents from "../components/docs/tableOfContents";
 import Tutorial from "../components/docs/tutorial";
+import "./tutorial.css"
 
 function Render() {
   const container = document.getElementById("root");
   const root = createRoot(container);
   root.render(
     <>
-      <TableOfContents/>
       <Tutorial/>
     </>
   );
