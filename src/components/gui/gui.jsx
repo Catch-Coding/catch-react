a7694ae895b9129e229178759503a9aeb74720c4
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 08:19:49 -0400

    this should work

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -7,17 +7,36 @@
 import React, { useRef } from 'react';
 import MenuBar from './../menu-bar/menu-bar.jsx';
 import CodeArea from './../code-area/code-area.jsx';
+import { fileOpen, fileSave } from "browser-fs-access";
 import "./gui.css";
 
 function newCallback() {};
 function saveCallback() {};
-function openCallback() {};
 
 function Gui({runtime}) {
     const codeAreaValue = useRef("");
     const setCodeAreaValue = useRef(()=>{}); // a function to overwrite the code area's value. will be used for opening a file.
     function runCallback() {
         runtime.sandboxRun(codeAreaValue.current);
+    };
+    async function openCallback() {
+        // set value function is in setCodeAreaValue.current.
+        const blob = await fileOpen({
+            extensions: [".ctch", ".json"],
+            description: "CatchJS Project File",
+            startIn: "downloads"
+        });
+        const reader = new FileReader(blob);
+        reader.onloadend = () => {
+            let obj;
+            try {
+                obj = JSON.parse(reader.result);
+            } catch(e) {
+                alert("Invalid CatchJS Project File")
+            }
+            setCodeAreaValue.current(obj.code)
+        };
+        reader.readAsText(blob);
     };
     return (
         <div className={"gui"}>
