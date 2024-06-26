d798b2a5c6cf3496f61b8ac4e78d30e3a84453f8
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-25 21:46:48 -0400

    Update code-area.jsx

--- src/components/code-area/code-area.jsx
+++ src/components/code-area/code-area.jsx
@@ -3,14 +3,15 @@
     written for Catch React by H3L
     LICENSED UNDER THE GPL V3
  */
-import React from 'react';
+import React, { useState } from 'react';
 import "./code-area.css";
 
 function CodeArea(props) {
+    const [value, setValue] = useState("")
     const { bindValue } = props;
     return (
         <div id="codeAreaContainer">
-            <textarea id="codeArea" placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value)}></textarea>
+            <textarea id="codeArea" value={value} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value, setValue(e.target.value))}></textarea>
         </div>
     );
 }
