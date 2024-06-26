ac68b7f3d07073fc79d21e071b1d45884e528a2f
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-25 21:54:21 -0400

    changes + realized that you dont need a rerender every time a change occurs.

--- src/components/code-area/code-area.jsx
+++ src/components/code-area/code-area.jsx
@@ -3,15 +3,24 @@
     written for Catch React by H3L
     LICENSED UNDER THE GPL V3
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./code-area.css";
 
 function CodeArea(props) {
-    const [value, setValue] = useState("")
-    const { bindValue } = props;
+    const [value, setValue] = useState("");
+    const { bindValue, setValue as overwriteRef } = props;
+    useEffect(()=>{
+      overwriteRef.current = (newVal) => {
+        bindValue.current = newVal;
+        setValue(newVal)
+      }
+      return () => {
+        overwriteRef.current = () => {};
+      }
+    })
     return (
         <div id="codeAreaContainer">
-            <textarea id="codeArea" value={value} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value, setValue(e.target.value))}></textarea>
+            <textarea id="codeArea"  value={value} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value)}></textarea>
         </div>
     );
 }
