a9dda1d069363c442ee6e98c8da0e66c9100e029
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 09:19:12 -0400

    stuff

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -4,15 +4,24 @@
     LICENSED UNDER THE GPL V3.
  */
 
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './menu-bar.css';
 import LogoSvg from '../../images/logo.svg';
 
-function MenuBar({newCallback, saveCallback, openCallback, runCallback}) {
+function MenuBar({newCallback, saveCallback, openCallback, runCallback, bindName, setName}) {
+    const [projectName, setProjectName] = useState("");
+    const ref = useRef("");
+    useEffect(()=>{
+        setName.current = (newVal) => {
+            ref.current = newVal;
+            setProjectName(newVal);
+        }
+        return () => (setName.current = () => {});
+    })
     return (
         <nav className="menu-bar">
                 <img src={LogoSvg}/>
-                <input type="text" placeholder="Project Name"></input>
+                <input type="text" value={ref.current} placeholder="Project Name" onChange={(e)=>(bindName.current = e.target.value, ref.current = e.target.value)}></input>
                 <button onClick={newCallback}>New</button>
                 <button onClick={saveCallback}>Save</button>
                 <button onClick={openCallback}>Open</button>
