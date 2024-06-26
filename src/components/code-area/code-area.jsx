0fadbc88ba84dac062297e4b3c4c88f27f0640af
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 10:34:29 -0400

    try doing this again

--- src/components/code-area/code-area.jsx
+++ src/components/code-area/code-area.jsx
@@ -22,7 +22,7 @@ function CodeArea(props) {
     })
     return (
         <div id="codeAreaContainer">
-            <textarea id="codeArea"  value={text.current} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), text.current = e.target.value, bindValue.current = e.target.value)}></textarea>
+            <textarea id="codeArea"  value={value} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), text.current = e.target.value, bindValue.current = e.target.value, setValue(e.target.value))}></textarea>
         </div>
     );
 }
