ca4ada1b5e94463a80f56a252745d582b483294b
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 14:48:18 -0400

    Update homepage.jsx

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -10,6 +10,7 @@ export default function Homepage() {
         <a href="studio">Studio</a>
         <a href="tutorial">Tutorials</a>
         <a href="https://discord.gg/btr8nNaWgc">Official Discord Server</a>
+        <a href="https://github.com/Catch-Coding/catch-react">Contribute</a>
       </nav>
       <h1>CatchJS Homepage</h1>
       <hr/>
@@ -17,6 +18,7 @@ export default function Homepage() {
       <p>In the future, CatchJS will include docs, tutorials, and an advanced studio.</p>
       <p>Currently, the website only includes a bare-bones studio and some tutorials.</p>
       <p>For now you can go to the <a href="studio">studio</a> or see <a href="tutorial">some tutorials</a>.</p>
+      <p>If you would like to help us develop our project or would like to report an issue, please go to our github repository: <a href="https://github.com/Catch-Coding/catch-react">https://github.com/Catch-Coding/catch-react</a> or join the official discord server.</p>
     </>
   )
 }
