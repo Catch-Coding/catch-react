da0578e45ffc5b6ba98c972704e88266f893161e
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 08:30:14 -0400

    hopefully this works cuz now it doesnt set value to just none

--- src/components/code-area/code-area.jsx
+++ src/components/code-area/code-area.jsx
@@ -3,15 +3,17 @@
     written for Catch React by H3L
     LICENSED UNDER THE GPL V3
  */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import "./code-area.css";
 
 function CodeArea(props) {
     const [value, setValue] = useState("");
+    const text = useRef("")
     const { bindValue, setValue : overwriteRef } = props;
     useEffect(()=>{
       overwriteRef.current = (newVal) => {
         bindValue.current = newVal;
+        text.current = newVal;
         setValue(newVal)
       }
       return () => {
@@ -20,7 +22,7 @@ function CodeArea(props) {
     })
     return (
         <div id="codeAreaContainer">
-            <textarea id="codeArea"  value={value} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value)}></textarea>
+            <textarea id="codeArea"  value={text.current} placeholder="CatchJS code goes here." autocorrect="off" onChange={(e)=>(console.log(props), bindValue.current = e.target.value, text.current = e.target.value)}></textarea>
         </div>
     );
 }
