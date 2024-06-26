86313c4ccc66d74349369abd94ae4a1b8fe19020
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 15:07:31 -0400

    bruh

--- src/components/docs/js.jsx
+++ src/components/docs/js.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import Code from "react-syntax-highlighter";
 import { a11yDark as dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-export default JavaScript({l, children}) {
+export default function JavaScript({l, children}) {
   return <Code language="javascript" showLineNumbers={l} style={dark}>{children}</Code>
 }
